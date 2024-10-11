b16ae4b5881acc028c1d47fc634c6a4b0a89dd2a
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-10-11 08:27:45 +0700

    register

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
         <div>
           <Link
             href="/pages/login"
-            className="bg-primary text-white px-4 py-2 rounded-lg mr-20 mt-5 mb-5 px-8"
+            className="bg-primary text-white py-2 rounded-lg mr-20 mt-5 mb-5 px-8"
           >
             Login
           </Link>
