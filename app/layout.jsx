7fbee4e18e194e630c473c2ba1d4aca434fe247d
nelifzyh <nelifauziyah07@gmail.com>
2024-10-16 11:03:57 +0700

    font

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body>
         {children}
       </body>
