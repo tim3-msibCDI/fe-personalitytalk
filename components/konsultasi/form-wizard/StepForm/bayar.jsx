cf17b08e9b0aff912bf2604e7f88cd29bfb74281
nelifzyh <nelifauziyah07@gmail.com>
2024-10-28 13:42:29 +0700

    component psikolog-keterangan transaksi

--- components/konsultasi/form-wizard/StepForm/bayar.jsx
+++ components/konsultasi/form-wizard/StepForm/bayar.jsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
+import Psikolog from "../psikolog";
+import PilihPembayaran from "../pilih_bayar";
+import Pembayaran from "../pembayaran";
 import { jadwalPsikolog } from "@/constants";
 import { useState } from "react";
-import Modal from "@/components/modals/modal";
-import SyaratKetentuan from "@/components/popup/snk";
-import Psikolog from "../psikolog";
 
 export default function FormBayar() {
-    //State untuk modal
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
-
+    
     // State untuk menyimpan data psikolog yang dipilih
-    const [selectedPsikolog, setSelectedPsikolog] = useState(jadwalPsikolog[0]); // Misalkan ambil psikolog pertama
-
-    //State untuk mengubah warna button ketika sudah menginput
-    const [inputValue, setInputValue] = useState("");
+    const [selectedPsikolog, setSelectedPsikolog] = useState(jadwalPsikolog[0]);
 
     return (
         <div className="p-6">
@@ -62,89 +55,14 @@ export default function FormBayar() {
                 </div>
                 {/* Konten Kanan */}
                 <div className="w-3/5">
-                    <div className="bg-primarylight2 rounded-lg p-4 mb-6">
-                        <h3 className="text-h3 font-semibold mb-4">Pembayaran</h3>
-                        {/* Dropdown pembayaran */}
-                        <div>
-                            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5">
-                                <option selected>Pilih Metode Pembayaran</option>
-                                <option value="BRI">Bank BRI</option>
-                                <option value="BNI">Bank BNI</option>
-                                <option value="BSI">Bank BSI</option>
-                                <option value="Mandiri">Bank Mandiri</option>
-                            </select>
-                        </div>
-                        <hr className="border-1 border-black mb-4 mt-2" />
-                        {/* Voucher */}
-                        <div>
-                            <label htmlFor="voucher" className="block mb-2 text-m font-semibold text-textcolor">
-                                Voucher
-                            </label>
-                            <div className="flex flex-1 gap-2">
-                                <input
-                                    type="text"
-                                    id="voucher"
-                                    aria-describedby="voucher-text-explanation"
-                                    className="bg-gray-50 border border-gray-300 text-text2 text-s rounded-lg block w-full p-2.5"
-                                    placeholder="Masukkan Voucher Anda"
-                                    value={inputValue}
-                                    onChange={(e) => setInputValue(e.target.value)}
-                                />
-                                <button
-                                    className={`rounded-lg text-whitebg text-s px-4 ${inputValue ? "bg-primary" : "bg-disable"}`}
-                                >
-                                    Reedem
-                                </button>
-                            </div>
-                        </div>
-                        {/* Harga */}
-                        <div className="text-m mt-3 space-y-2">
-                            <div className="flex justify-between">
-                                <p>Total Harga Konsultasi</p>
-                                <p><b>Rp 50.000</b></p>
-                            </div>
-                            <div className="flex justify-between">
-                                <p>Total Diskon Voucher</p>
-                                <p><b>Rp 50.000</b></p>
-                            </div>
-                        </div>
-                        <hr className="border-1 border-black mb-4 mt-2" />
-                        <div className="text-h3 font-semibold flex justify-between">
-                            <p>Total Biaya</p>
-                            <p>Rp 50.000</p>
-                        </div>
-                        {/* SNK */}
-                        <div className="mt-4">
-                            <div className="flex items-center mb-4">
-                                <input
-                                    id="checkbox"
-                                    type="checkbox"
-                                    className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary focus:ring-2"
-                                />
-                                <label htmlFor="checkbox" className="ml-2 text-sm font-medium text-gray-900">
-                                    Saya telah membaca dan menyetujui{" "}
-                                    <span
-                                        className="text-primary cursor-pointer underline"
-                                        onClick={openModal}
-                                    >
-                                        Syarat & Ketentuan
-                                    </span>
-                                </label>
-                            </div>
-                        </div>
-                        {/* Button bayar */}
-                        <button
-                            className={`text-m w-full py-2 rounded-md font-semibold text-whitebg ${inputValue ? "bg-primary" : "bg-disable"}`}
-                        >
-                            Bayar
-                        </button>
-                    </div>
+                    {/* Tampilkan PilihPembayaran jika status_pembayaran null, sebaliknya tampilkan Pembayaran */}
+                    {selectedPsikolog.status_pembayaran === null ? (
+                        <PilihPembayaran />
+                    ) : (
+                        <Pembayaran />
+                    )}
                 </div>
             </div>
-            {/* Modal component */}
-            <Modal isOpen={isModalOpen} onClose={closeModal}>
-                <SyaratKetentuan onClose={closeModal} /> {/* Mengirim fungsi closeModal sebagai prop */}
-            </Modal>
         </div>
     );
 }
---
+++ components/konsultasi/form-wizard/pembayaran.jsx
@@ -0,0 +1,25 @@
+export default function Pembayaran() {
+    return (
+        <div>
+            {/* Rincian */}
+            <div>
+                {/* Detail Pemesanan */}
+                <div>
+                    <p className="text-m font-semibold">Detail Pemesanan</p>
+                    <div>
+                        <p>No. Pemesanan</p>
+                        <p>: 1222332545</p>
+                    </div>
+                    <div>
+                        <p>Metode Pembayaran</p>
+                        <p>: QRIS</p>
+                    </div>
+                </div>
+                {/* Rincian nota */}
+                <div>
+                    <p className="text-m font-semibold">Detail Pemesanan</p>
+                </div>
+            </div>
+        </div>
+    );
+}
