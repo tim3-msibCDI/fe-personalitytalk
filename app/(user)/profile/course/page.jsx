233201dca798ee4df25ec9c83eff5152e659413e
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-10-28 15:23:36 +0700

    halaman riwayat course

--- app/(user)/profile/course/page.jsx
+++ app/(user)/profile/course/page.jsx
@@ -1,5 +1,42 @@
-export default function CoursePage() {
+"use client"
+import { useState } from "react";
+import ListKonsultasi from "@/components/card/listkonsultasi";
+import ListTransaksi from "@/components/card/listtransaksi";
+
+export default function CourseUserPage() {
+  const [activeTab, setActiveTab] = useState("konsultasi"); // State untuk tab aktif
+
   return (
-    <div>Course</div>
-  )
+    <div className="w-full">
+      <div className="w-full flex justify-between items-start self-stretch mb-9">
+        <h3 className="text-h3 font-semibold">Course Saya</h3>
+      </div>
+      <div className="">
+        <ul className="flex space-x-4">
+          <li
+            className={`py-2 px-4 cursor-pointer text-primary ${
+              activeTab === "konsultasi" ? "bg-primarylight rounded-t-lg font-semibold" : ""
+            }`}
+            onClick={() => setActiveTab("konsultasi")}
+          >
+            Riwayat Course
+          </li>
+          <li
+            className={`py-2 px-4 cursor-pointer text-primary ${
+              activeTab === "transaksi" ? "bg-primarylight rounded-t-lg font-semibold" : ""
+            }`}
+            onClick={() => setActiveTab("transaksi")}
+          >
+            Riwayat Transaksi
+          </li>
+        </ul>
+        <div className={`bg-primarylight p-2 ${
+              activeTab === "konsultasi" ? "rounded-tr-lg rounded-br-lg" : "rounded-lg"
+            }`}>
+          {activeTab === "konsultasi" && <ListKonsultasi />}
+          {activeTab === "transaksi" && <ListTransaksi />}
+        </div>
+      </div>
+    </div>
+  );
 }
