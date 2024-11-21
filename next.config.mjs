939694c5f0f1fde7875e29314ee4415a29878032
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-11-21 08:44:55 +0700

    icon image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images:{
+      domains: ['127.0.0.1'],
+    },
+  };
+  
+  export default nextConfig;
+  
