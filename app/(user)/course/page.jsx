9eb54f8196809d012f77ce57af580e084bda5eec
Ardhilla Eka <86395605+dwlndr@users.noreply.github.com>
2024-12-24 09:07:00 +0700

    Update page.jsx

--- app/(user)/course/page.jsx
+++ app/(user)/course/page.jsx
@@ -1,8 +1,8 @@
 // pages/Course.js
 
 import Image from "next/image";
-import CourseList from "@/components/card/CourseList";
-import ListCourse from "@/components/card/ListCourse";
+import CourseList from "@/components/card/courselist";
+import ListCourse from "@/components/card/listcourse";
 import { coursesData } from "@/constants";
 
 export default function Course() {
